b7a35b90223fe323d0db1320657c2aea255ac1aa
bajoelabrigo <bajoelabrigo@gmail.com>
2025-06-06 01:29:23 -0500

    update cors

--- backend/app.js
+++ backend/app.js
@@ -21,11 +21,7 @@ mongoose
 
 //! Cors config
 const corsOptions = {
-  origin: [
-    "https://controldegastoschurch.netlify.app",
-    "https://mern-expenses-track-frontend.onrender.com",
-    "http://localhost:5173",
-  ],
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
