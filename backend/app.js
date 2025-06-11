81ecd1e04955e54ac7c11b969ede324698ff43a1
bajoelabrigo <bajoelabrigo@gmail.com>
2025-06-11 13:14:47 -0500

    app backend

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,10 @@ mongoose
 
 //! Cors config
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://controldegastosiglesia.netlify.app",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
